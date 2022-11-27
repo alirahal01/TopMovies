de4a19bd8482252811f1cd8669d611f2a6417693
ali rahal <ali.rahal01@gmail.com>
2022-11-27 16:27:43 +0200

    add HTTPMethod

---
+++ TopMovies/TopMovies/Services/HTTPMethod.swift
@@ -0,0 +1,16 @@
+//
+//  HTTPMethod.swift
+//  TopMovies
+//
+//  Created by ali rahal on 27/11/2022.
+//
+
+import Foundation
+
+enum HTTPMethod : String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+    case patch = "PATCH"
+}
